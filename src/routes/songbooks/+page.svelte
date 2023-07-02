a43117bf95c33cb346a5b2f5da9249d4f8470fff
ValentG2 <valentin.gregoire@basf.com>
2023-07-02 09:27:21 +0200

    test with api routes

--- src/routes/songbooks/+page.svelte
+++ src/routes/songbooks/+page.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+  import type { Songbook } from "../../models/songbook.model";
+
+  let songbooks: Songbook[];
 
   async function getStuff() {
     const response = await fetch("/songbooks", {
@@ -15,7 +18,29 @@
     console.log("data", data);
 
   }
+
+  async function getSongbooks() {
+    const response = await fetch("/songbooks", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+    console.log("songbooks", data);
+    songbooks = data;
+  }
 </script>
 
 <h1>Songbooks</h1>
 <button on:click={getStuff}>Get Stuff</button>
+<button class="btn btn-primary" on:click={getSongbooks}>Get Songbooks</button>
+
+{#if songbooks}
+  <ul>
+    {#each songbooks as songbook}
+      <li>{songbook.name}</li>
+    {/each}
+  </ul>
+{/if}
--- src/routes/songbooks/+server.ts
+++ src/routes/songbooks/+server.ts
@@ -1,9 +1,28 @@
 import { json } from "@sveltejs/kit";
+import fs from "fs";
+import type { Songbook } from "../../models/songbook.model";
+
+const SONGBOOKS_PATH = "static/songbooks/";
 
 export async function POST({ request }) {
-  console.log("type", typeof request);
   const requestJson = await request.json();
   console.log("json", requestJson);
   const name: string = requestJson.name;
   return json(name.toUpperCase());
+}
+
+export async function PUT() {
+  let songbooks: Songbook[] = [];
+  const songbookNames = fs
+    .readdirSync(SONGBOOKS_PATH, { withFileTypes: true })
+    .filter((item) => !item.isDirectory())
+    .map((item) => item.name.split(".")[0]);
+  songbookNames.forEach((songbookName: string) => {
+    const songbook = JSON.parse(
+      fs.readFileSync(`${SONGBOOKS_PATH}/${songbookName}.json`).toString()
+    );
+    songbooks.push(songbook);
+    console.log(songbook);
+  });
+  return json(songbooks);
 }
